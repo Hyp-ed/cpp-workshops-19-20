7ac0f84ed2bbff6db40720aadeb21ca40a80e46d
gregorydayao12 <s1814658@ed.ac.uk>
2019-10-06 01:43:43 +0100

    rename main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include "rectangle.hpp"
 #include "utils/logger.hpp"
+#include "rectangle.hpp"
 
 using namespace std;
-using namespace hyped::Rectangle;
 using hyped::utils::Logger;
+using namespace hyped::Rectangle;
 
 int main() {
-
   Logger log_(true, 3);
+
   int height = 0;
   int width = 0;
   log_.INFO("MAIN", "Input height of your rectangle type int: ");
@@ -29,6 +29,6 @@ int main() {
   rec1.setWidth(width);
   log_.INFO("MAIN", "Area of your new rectangle: %d", rec1.getArea());
   log_.INFO("MAIN", "Expected area: %d", height*width);
-  
+
   return 0;
 }
