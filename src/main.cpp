9ed9417c336657017b199aff0f80eced0f76dd62
gregorydayao12 <s1814658@ed.ac.uk>
2019-10-06 01:40:49 +0100

    add main

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "utils/logger.hpp"
+
+using namespace std;
+using hyped::utils::Logger;
+
+int main() {
+  Logger log_(true, 3);
+
+  // test your state machine transitions
+
+  return 0;
+}
