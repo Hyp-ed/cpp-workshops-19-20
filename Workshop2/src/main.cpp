3b635c0f5692619a6157aee88b0a92339afde050
Martin Kristien <martin.kristien@gmail.com>
2019-10-11 17:56:29 +0200

    Propose changes to exercise-3 with possible solution

--- Workshop2/src/main.cpp
+++ Workshop2/src/main.cpp
@@ -1,34 +1,34 @@
 #include <iostream>
 #include "utils/logger.hpp"
-#include "rectangle.hpp"
+#include "rectangle/rectangle.hpp"
 
-using namespace std;
+using std::cin;
 using hyped::utils::Logger;
-using namespace hyped::Rectangle;
+using hyped::rectangle::Rectangle;
 
 int main() {
-  Logger log_(true, 3);
+  Logger log(true, 3);
 
-  int height = 0;
-  int width = 0;
-  log_.INFO("MAIN", "Input height of your rectangle type int: ");
+  int height  = 0;
+  int width   = 0;
+  log.INFO("MAIN", "Input height of your rectangle type int: ");
   cin >> height;
-  log_.INFO("MAIN", "Input width of your rectangle type int: ");
+  log.INFO("MAIN", "Input width of your rectangle type int: ");
   cin >> width;
 
-  Rectangle rec1(height,width);
-  log_.INFO("MAIN", "Area of your rectangle: %d", rec1.getArea());
-  log_.INFO("MAIN", "Expected area: %d", height*width);
+  Rectangle rec1(height, width);
+  log.INFO("MAIN", "Area of your rectangle: %d", rec1.getArea());
+  log.INFO("MAIN", "Expected area: %d", height*width);
 
-  log_.INFO("MAIN", "Input new height of your rectangle type int: ");
+  log.INFO("MAIN", "Input new height of your rectangle type int: ");
   cin >> height;
-  log_.INFO("MAIN", "Input width of your rectangle type int: ");
+  log.INFO("MAIN", "Input width of your rectangle type int: ");
   cin >> width;
 
   rec1.setHeight(height);
   rec1.setWidth(width);
-  log_.INFO("MAIN", "Area of your new rectangle: %d", rec1.getArea());
-  log_.INFO("MAIN", "Expected area: %d", height*width);
+  log.INFO("MAIN", "Area of your new rectangle: %d", rec1.getArea());
+  log.INFO("MAIN", "Expected area: %d", height*width);
 
   return 0;
 }
