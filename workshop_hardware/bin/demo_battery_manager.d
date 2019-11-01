38eda8dcf79f2ae88c9658e4702ce2f0d8f34e0e
gregorydayao12 <s1814658@ed.ac.uk>
2019-11-01 20:18:36 +0000

    last workshop

---
+++ workshop_hardware/bin/demo_battery_manager.d
@@ -0,0 +1,36 @@
+bin/demo_battery_manager.o: src/demo_battery_manager.cpp \
+  src/sensors/bms_manager.hpp src/sensors/manager_interface.hpp \
+  src/data/data.hpp src/utils/math/vector.hpp src/data/data_point.hpp \
+  src/utils/concurrent/lock.hpp src/utils/concurrent/thread.hpp \
+  src/utils/logger.hpp src/sensors/interface.hpp src/utils/system.hpp \
+  src/utils/concurrent/barrier.hpp \
+  src/utils/concurrent/condition_variable.hpp src/utils/utils.hpp \
+  src/utils/io/gpio.hpp
+
+src/sensors/bms_manager.hpp:
+
+src/sensors/manager_interface.hpp:
+
+src/data/data.hpp:
+
+src/utils/math/vector.hpp:
+
+src/data/data_point.hpp:
+
+src/utils/concurrent/lock.hpp:
+
+src/utils/concurrent/thread.hpp:
+
+src/utils/logger.hpp:
+
+src/sensors/interface.hpp:
+
+src/utils/system.hpp:
+
+src/utils/concurrent/barrier.hpp:
+
+src/utils/concurrent/condition_variable.hpp:
+
+src/utils/utils.hpp:
+
+src/utils/io/gpio.hpp:
---
+++ workshop_hardware/src/sensors/bms_manager.cpp
@@ -0,0 +1,165 @@
+/*
+ * Author: Gregory Dayao and Jack Horsburgh
+ * Organisation: HYPED
+ * Date: 20/06/18
+ * Description:
+ * BMS manager for getting battery data and pushes to data struct.
+ * Checks whether batteries are in range and enters emergency state if fails.
+ *
+ *    Copyright 2019 HYPED
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
+#include "sensors/bms_manager.hpp"
+
+#include "utils/timer.hpp"
+#include "sensors/fake_batteries.hpp"
+#include "utils/config.hpp"
+
+namespace hyped {
+namespace sensors {
+
+BmsManager::BmsManager(Logger& log)
+    : ManagerInterface(log),
+      sys_(utils::System::getSystem()),
+      data_(Data::getInstance())
+{
+  old_timestamp_ = utils::Timer::getTimeMicros();
+  // fake batteries here
+  for (int i = 0; i < data::Batteries::kNumLPBatteries; i++) {
+    bms_[i] = new FakeBatteries(log_, true, false);
+  }
+  for (int i = 0; i < data::Batteries::kNumHPBatteries; i++) {
+    bms_[i + data::Batteries::kNumLPBatteries] = new FakeBatteries(log_, false, false);
+  }
+
+
+  previous_state_ = data_.getStateMachineData().current_state;
+  // kInit for SM transition
+  batteries_ = data_.getBatteriesData();
+  batteries_.module_status = data::ModuleStatus::kInit;
+  data_.setBatteriesData(batteries_);
+  Thread::yield();
+  start_time_ = utils::Timer::getTimeMicros();
+  log_.INFO("BMS-MANAGER", "batteries data has been initialised");
+}
+
+void BmsManager::run()
+{
+  while (sys_.running_) {
+    // keep updating data_ based on values read from sensors
+    for (int i = 0; i < data::Batteries::kNumLPBatteries; i++) {
+      bms_[i]->getData(&batteries_.low_power_batteries[i]);
+      if (!bms_[i]->isOnline())
+        batteries_.low_power_batteries[i].voltage = 0;
+    }
+    for (int i = 0; i < data::Batteries::kNumHPBatteries; i++) {
+      bms_[i + data::Batteries::kNumLPBatteries]->getData(&batteries_.high_power_batteries[i]);
+      if (!bms_[i + data::Batteries::kNumLPBatteries]->isOnline())
+        batteries_.high_power_batteries[i].voltage = 0;
+    }
+
+    data::State state = data_.getStateMachineData().current_state;
+    if (utils::Timer::getTimeMicros() - start_time_ > check_time_) {
+      // check health of batteries
+      if (batteries_.module_status != data::ModuleStatus::kCriticalFailure) {
+        if (!batteriesInRange()) {
+          if (batteries_.module_status != previous_status_)
+            log_.ERR("BMS-MANAGER", "battery failure detected");
+          batteries_.module_status = data::ModuleStatus::kCriticalFailure;
+        }
+        previous_status_ = batteries_.module_status;
+      }
+    }
+
+    // publish the new data
+    data_.setBatteriesData(batteries_);
+    previous_state_ = state;
+    sleep(100);
+  }
+}
+
+bool BmsManager::batteriesInRange()
+{
+  // check LP
+  for (int i = 0; i < data::Batteries::kNumLPBatteries; i++) {
+    auto& battery = batteries_.low_power_batteries[i];      // reference batteries individually
+    if (battery.voltage < 175 || battery.voltage > 294) {   // voltage in 17.5V to 29.4V
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS LP %d voltage out of range: %d", i, battery.voltage);
+      return false;
+    }
+
+    if (battery.current < 0 || battery.current > 500) {       // current in 0A to 50A
+       if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS LP %d current out of range: %d", i, battery.current);
+      return false;
+    }
+
+    if (battery.average_temperature < 10 || battery.average_temperature > 60) {  // temperature in 10C to 60C NOLINT[whitespace/line_length]
+       if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS LP %d temperature out of range: %d", i, battery.average_temperature); // NOLINT[whitespace/line_length]
+      return false;
+    }
+
+    if (battery.charge < 20 || battery.charge > 100) {  // charge in 20% to 100%
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS LP %d charge out of range: %d", i, battery.charge);
+      return false;
+    }
+  }
+
+  // check HP
+  for (int i = 0; i < data::Batteries::kNumHPBatteries; i++) {
+    auto& battery = batteries_.high_power_batteries[i];     // reference battereis individually
+    if (battery.voltage < 1000 || battery.voltage > 1296) {   // voltage in 100V to 129.6V
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS HP %d voltage out of range: %d", i, battery.voltage);
+      return false;
+    }
+
+    if (battery.current < 0 || battery.current > 3500) {  // current in 0A to 350A
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS HP %d current out of range: %d", i, battery.current);
+      return false;
+    }
+
+    if (battery.average_temperature < 10 || battery.average_temperature > 65) {  // temperature in 10C to 65C NOLINT[whitespace/line_length]
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS HP %d temperature out of range: %d", i, battery.average_temperature); // NOLINT[whitespace/line_length]
+      return false;
+    }
+
+    if (battery.low_temperature < 10) {
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS HP %d temperature out of range: %d", i, battery.low_temperature); // NOLINT[whitespace/line_length]
+      return false;
+    }
+
+    if (battery.high_temperature > 65) {
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS HP %d temperature out of range: %d", i, battery.high_temperature); // NOLINT[whitespace/line_length]
+      return false;
+    }
+
+    if (battery.charge < 20 || battery.charge > 100) {  // charge in 20% to 100%
+      if (batteries_.module_status != previous_status_)
+        log_.ERR("BMS-MANAGER", "BMS HP %d charge out of range: %d", i, battery.charge);
+      return false;
+    }
+  }
+  return true;
+}
+
+}}  // namespace hyped::sensors
